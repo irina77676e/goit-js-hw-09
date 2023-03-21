25b6f1fb49039e1d89c3fb5751a6a87c3aef9c9a
irina77676e <114076050+irina77676e@users.noreply.github.com>
2023-03-21 18:01:39 +0000

    Deploying to gh-pages from @ irina77676e/goit-js-hw-09@9ea2bd22af2a2f4a44d8e767de5a6ad195308f58 🚀

---
+++ 01-color-switcher.2e047c94.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let a=null;function d(){return document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(()=>{a=setInterval(d,1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(a),e.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.2e047c94.js.map
