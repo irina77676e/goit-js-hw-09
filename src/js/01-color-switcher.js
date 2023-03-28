9f2fb7fb4d590dcf56c801703e0077fd814860e6
irina77676e <114076050+irina77676e@users.noreply.github.com>
2023-03-28 20:26:24 +0200

    fixed mistakes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,23 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 
-let timerId = null;
+ let timerId = null;
+stopBtn.disabled = true;
 
 startBtn.addEventListener('click', () => {
-   timerId = setInterval(getRandomHexColor, 1000);
-    startBtn.disabled = true;
+ timerId = setInterval(getRandomHexColor, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  
 })
 
 function getRandomHexColor() {
   return document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
+
 stopBtn.addEventListener('click', () => {
     clearInterval(timerId);
     stopBtn.disabled = true;
-    startBtn.disabled = false;
+  startBtn.disabled = false;
 })
